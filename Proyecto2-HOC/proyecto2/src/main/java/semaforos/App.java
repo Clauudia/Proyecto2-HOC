598906463c7d4b1eef87524022e79c21931867da
Claudia <claugaro@ciencias.unam.mx>
2017-05-23 22:08:35 -0500

    Creé la estructura con maven

---
+++ Proyecto2-HOC/proyecto2/src/main/java/semaforos/App.java
@@ -0,0 +1,13 @@
+package semaforos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
